81ec723a1dcb4d7dd6f264a11c314978604ac044
Evan Hazlett <ejhazlett@gmail.com>
2014-04-10 23:19:24 -0400

    removed custom hosts file

--- run.sh
+++ run.sh
@@ -4,17 +4,6 @@ MYSQL_CFG_URL=${MYSQL_CFG_URL:-}
 DATA_DIR=${DATA_DIR:-/var/lib/mysql}
 MYSQL_OPTS=${MYSQL_OPTS:-}
 
-cat << EOF > /etc/hosts
-127.0.0.1   localhost $HOSTNAME
-
-::1 ip6-localhost ip6-loopback
-fe00::0 ip6-localnet
-ff00::0 ip6-mcastprefix
-ff02::1 ip6-allnodes
-ff02::2 ip6-allrouters
-ff02::3 ip6-allhosts
-EOF
-
 if [ ! -z "$MYSQL_CFG_URL" ] ; then
     wget $MYSQL_CFG_URL -O /etc/mysql/my.cnf
 fi
